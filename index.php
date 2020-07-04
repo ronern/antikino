f700bb4b2c2491e397e4b49ccccf1ad85cb219e7
Dmitriy Volkov <dumac89@gmail.com>
2020-07-04 13:20:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -57,3 +57,43 @@ $sign = strtoupper(hash('sha256', implode(':', $arHash)));
 */ ?>
 <input type="submit" name="m_process" value="send" />
 </form>
+
+
+<script id="widget-wfp-script" language="javascript" type="text/javascript" src="https://secure.wayforpay.com/server/pay-widget.js"></script>
+<script type="text/javascript">
+    var wayforpay = new Wayforpay();
+    var pay = function () {
+        wayforpay.run({
+                merchantAccount : "test_merch_n1",
+                merchantDomainName : "www.market.ua",
+                authorizationType : "SimpleSignature",
+                merchantSignature : "b95932786cbe243a76b014846b63fe92",
+                orderReference : "DH783023",
+                orderDate : "1415379863",
+                amount : "1547.36",
+                currency : "UAH",
+                productName : "Процессор Intel Core i5-4670 3.4GHz",
+                productPrice : "1000",
+                productCount : "1",
+                clientFirstName : "Вася",
+                clientLastName : "Васечкин",
+                clientEmail : "some@mail.com",
+                clientPhone: "380631234567",
+		straightWidget: true,
+                language: "UA"
+            },
+            function (response) {
+                // on approved             
+            },
+            function (response) {
+                // on declined
+            },
+            function (response) {
+                // on pending or in processing
+            }
+        );
+    }
+</script>
+ 
+ 
+<button type="button" onclick="pay();">Оплатить</button>
